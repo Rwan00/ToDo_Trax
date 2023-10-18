d7d1237ac17f15405ba11cd9962ca460b62ac922
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2023-10-19 01:06:25 +0300

    Add Task Tile

--- lib/ui/widgets/task_tile.dart
+++ lib/ui/widgets/task_tile.dart
@@ -12,8 +12,8 @@ class TaskTile extends StatelessWidget {
       height: 110,
       //margin: EdgeInsets.all(16),
       decoration: BoxDecoration(
-          border: Border.all(color: const Color(0xffBFBFBF),width: 0.3),
-          borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: const Color(0xffBFBFBF),width: 0.3),
+        borderRadius: BorderRadius.circular(8),
         color:  Get.isDarkMode? const Color.fromRGBO(44, 44, 44, 1):Colors.white,
       ),
       child: Row(
@@ -43,7 +43,7 @@ class TaskTile extends StatelessWidget {
               Row(
                 children: <Widget>[
                   const Icon(Icons.access_time_rounded,
-                      color: Color.fromRGBO(119, 119, 119, 1),size: 12,),
+                    color: Color.fromRGBO(119, 119, 119, 1),size: 12,),
                   const SizedBox(
                     width: 4,
                   ),
@@ -54,36 +54,36 @@ class TaskTile extends StatelessWidget {
                 ],
               ),
               const SizedBox(height: 7,),
-             Row(
-               children: <Widget>[
-                 const SizedBox(width: 10,),
-                 Text("Due Date : October 25",style: subHeading,),
+              Row(
+                children: <Widget>[
+                  const SizedBox(width: 10,),
+                  Text("Due Date : October 25",style: subHeading,),
 
-               ],
-             ),
+                ],
+              ),
             ],
           ),
           const SizedBox(width: 120,),
           Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0, bottom: 7),
-                child: Container(
-                  width: 50,
-                  height: 18,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Get.isDarkMode? const Color.fromRGBO(169, 169, 169, 1):const Color.fromRGBO(242, 237, 237, 1)
-                  ),
-                  child: Center(
-                    child: Text("Low",style: subHeading.copyWith(color: Colors.black),),
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0, bottom: 7),
+                  child: Container(
+                    width: 50,
+                    height: 18,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Get.isDarkMode? const Color.fromRGBO(169, 169, 169, 1):const Color.fromRGBO(242, 237, 237, 1)
+                    ),
+                    child: Center(
+                      child: Text("Low",style: subHeading.copyWith(color: Colors.black),),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 37,),
-              Text("Completed",style: subHeading.copyWith(color: const Color.fromRGBO(20, 99, 0, 1)),),
-              const SizedBox(height: 16,),
-            ],
+                const SizedBox(height: 37,),
+                Text("Completed",style: subHeading.copyWith(color: const Color.fromRGBO(20, 99, 0, 1)),),
+                const SizedBox(height: 16,),
+              ]
           )
         ],
       ),
