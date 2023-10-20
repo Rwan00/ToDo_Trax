52c61f15ff1c5325282592cc08ca4ded68713e72
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2023-10-20 19:09:09 +0300

    Add custom show_Dialog

--- lib/ui/views/ongoing_view.dart
+++ lib/ui/views/ongoing_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-
 import '../methods/app_bar.dart';
 import '../methods/show_ongoing_tasks.dart';
 import '../methods/task_added_dialog.dart';
