7ebf544525d446f84ae8bf12df097a2b27e0605a
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2023-10-29 19:31:29 +0200

    init Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:todo_trax/services/theme_service.dart';
 import 'package:todo_trax/ui/theme.dart';
 import 'package:todo_trax/ui/views/ongoing_view.dart';
@@ -10,6 +11,8 @@ void main() async{
 
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  await Hive.initFlutter();
+  await Hive.openBox("notes_box");
   runApp(const ToDoTrax());
 }
 
--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,13 +1,28 @@
-class Task{
+import 'package:hive/hive.dart';
+
+part 'task.g.dart';
+
+@HiveType(typeId: 0)
+class Task extends HiveObject{
+  @HiveField(0)
   int? id;
+  @HiveField(1)
   String? title;
+  @HiveField(2)
   int? purpose;
+  @HiveField(3)
   String? date;
+  @HiveField(4)
   String? startTime;
+  @HiveField(5)
   String? endTime;
+  @HiveField(6)
   int? repeat;
+  @HiveField(7)
   String? description;
+  @HiveField(8)
   int? reminder;
+  @HiveField(9)
   int? isCompleted;
 
   Task({
