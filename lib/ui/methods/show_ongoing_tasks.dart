3d3e66c9ba0bcc2769003487616d49d329448762
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2023-11-02 20:28:28 +0200

    Display Date Properly

--- lib/ui/methods/show_ongoing_tasks.dart
+++ lib/ui/methods/show_ongoing_tasks.dart
@@ -4,6 +4,7 @@ import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:todo_trax/cubits/read_tasks_cubit/read_task_cubit.dart';
 import 'package:todo_trax/ui/methods/show_dialog.dart';
 import 'package:todo_trax/ui/methods/task_added_dialog.dart';
@@ -23,21 +24,28 @@ showOngoingTasks() {
       return ListView.builder(
           itemCount: tasks.length,
           itemBuilder: (BuildContext context, int index) {
-            final item = tasks[index];
-            return AnimationConfiguration.staggeredList(
-              duration: const Duration(milliseconds: 500),
-              position: index,
-              child: SlideAnimation(
-                horizontalOffset: 100,
-                child: FadeInAnimation(
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.only(top: 8.0, bottom: 8, right: 24),
-                    child: OngoingTaskTile(task: tasks[index]),
+            if(tasks[index].repeat == 0 ||
+                tasks[index].date == DateFormat('MMMM d').format(DateTime.now()) &&
+            tasks[index].isCompleted == 0
+            ) {
+              return AnimationConfiguration.staggeredList(
+                duration: const Duration(milliseconds: 500),
+                position: index,
+                child: SlideAnimation(
+                  horizontalOffset: 100,
+                  child: FadeInAnimation(
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.only(top: 8.0, bottom: 8, right: 24),
+                      child: OngoingTaskTile(task: tasks[index]),
+                    ),
                   ),
                 ),
-              ),
-            );
+              );
+            }
+            else {
+              return Container();
+            }
           });
     }
   });
--- lib/ui/methods/show_your_tasks.dart
+++ lib/ui/methods/show_your_tasks.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:todo_trax/ui/methods/show_dialog.dart';
 import 'package:todo_trax/ui/methods/task_added_dialog.dart';
 
@@ -12,7 +13,7 @@ import '../widgets/ongoing_task_tile.dart';
 import '../widgets/task_tile.dart';
 import 'no_task_message.dart';
 
-showYourTasks() {
+showYourTasks(DateTime myDate) {
   return BlocBuilder<TasksCubit, TasksState>(builder: (context, state) {
     List<Task> tasks = BlocProvider.of<TasksCubit>(context).tasks ?? [];
     if (tasks.isEmpty) {
@@ -22,88 +23,105 @@ showYourTasks() {
       return ListView.builder(
           itemCount: tasks.length,
           itemBuilder: (BuildContext context, int index) {
-            final item = tasks[index];
-            return AnimationConfiguration.staggeredList(
-              duration: const Duration(milliseconds: 500),
-              position: index,
-              child: SlideAnimation(
-                horizontalOffset: 100,
-                child: FadeInAnimation(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 8.0, bottom: 8, right: 24),
-                    child: Dismissible(
-                      key: Key(item.title!),
-                      confirmDismiss: (DismissDirection dir) async {
-                        if (dir == DismissDirection.startToEnd) {
-                          final bool result = await buildAlertDialog(
-                              context: context,
-                              title: 'Delete ${item.title} Task',
-                              content: 'Are You Sure You Want To Delete ${item.title} Task?',
-                              onTap: () {
-                                Get.back();
-                              },
-                              q1: 'Delete',
-                              clr: Colors.red
-                          );
-                          return result;
-                        } else {
-                          final bool result = await buildAlertDialog(
-                              context: context,
-                              title: 'Complete ${item.title} Task',
-                              content: 'Did You Complete ${item.title} Task?',
-                              onTap: () {
-                                tasks[index].isCompleted = 1;
-                                Get.back();
-                                buildDialog(
-                                  context,
-                                  imgUrl: 'assets/images/congrats.gif',
-                                  titleTxt: 'Congratulations',
-                                  subTitleTxt: 'You Successfully completed your task. It’s a great step for better life waiting for you.',
-                                );
-                                //Navigator.of(context).pop();
-                              },
-                              q1: 'Complete',
-                              clr: Colors.green
-                          );
-                          return result;
-                        }
-                      },
-                      background: Container(
-                        height: 95,
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: const Color(0xffBFBFBF), width: 0.3),
-                            borderRadius: BorderRadius.circular(8),
-                            color: const Color.fromRGBO(169, 30, 0, 1)),
-                        child: Align(
-                          alignment: const Alignment(-0.9, 0),
-                          child: Text(
-                            "Canceled",
-                            style: txtDis,
+           if (tasks[index].repeat ==  0  ||
+                tasks[index].date == DateFormat.yMd().format(myDate) ||
+                (tasks[index].repeat == 1 &&
+                    myDate
+                        .difference(
+                        DateFormat('MMMM d').parse(tasks[index].date!))
+                        .inDays %
+                        7 ==
+                        0) ||
+                (tasks[index].repeat == 2 &&
+                    DateFormat('MMMM d').parse(tasks[index].date!).day ==
+                        myDate.day)) {
+              final item = tasks[index];
+              return AnimationConfiguration.staggeredList(
+                duration: const Duration(milliseconds: 500),
+                position: index,
+                child: SlideAnimation(
+                  horizontalOffset: 100,
+                  child: FadeInAnimation(
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.only(top: 8.0, bottom: 8, right: 24),
+                      child: Dismissible(
+                        key: Key(item.title!),
+                        confirmDismiss: (DismissDirection dir) async {
+                          if (dir == DismissDirection.startToEnd) {
+                            final bool result = await buildAlertDialog(
+                                context: context,
+                                title: 'Delete ${item.title} Task',
+                                content:
+                                    'Are You Sure You Want To Delete ${item.title} Task?',
+                                onTap: () {
+                                  Get.back();
+                                },
+                                q1: 'Delete',
+                                clr: Colors.red);
+                            return result;
+                          } else {
+                            final bool result = await buildAlertDialog(
+                                context: context,
+                                title: 'Complete ${item.title} Task',
+                                content: 'Did You Complete ${item.title} Task?',
+                                onTap: () {
+                                  tasks[index].isCompleted = 1;
+                                  Get.back();
+                                  buildDialog(
+                                    context,
+                                    imgUrl: 'assets/images/congrats.gif',
+                                    titleTxt: 'Congratulations',
+                                    subTitleTxt:
+                                        'You Successfully completed your task. It’s a great step for better life waiting for you.',
+                                  );
+                                  //Navigator.of(context).pop();
+                                },
+                                q1: 'Complete',
+                                clr: Colors.green);
+                            return result;
+                          }
+                        },
+                        background: Container(
+                          height: 95,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: const Color(0xffBFBFBF), width: 0.3),
+                              borderRadius: BorderRadius.circular(8),
+                              color: const Color.fromRGBO(169, 30, 0, 1)),
+                          child: Align(
+                            alignment: const Alignment(-0.9, 0),
+                            child: Text(
+                              "Canceled",
+                              style: txtDis,
+                            ),
                           ),
                         ),
-                      ),
-                      secondaryBackground: Container(
-                        height: 95,
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: const Color(0xffBFBFBF), width: 0.3),
-                            borderRadius: BorderRadius.circular(8),
-                            color: const Color.fromRGBO(0, 169, 37, 1)),
-                        child: Align(
-                          alignment: const Alignment(0.9, 0),
-                          child: Text(
-                            "Completed",
-                            style: txtDis,
+                        secondaryBackground: Container(
+                          height: 95,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: const Color(0xffBFBFBF), width: 0.3),
+                              borderRadius: BorderRadius.circular(8),
+                              color: const Color.fromRGBO(0, 169, 37, 1)),
+                          child: Align(
+                            alignment: const Alignment(0.9, 0),
+                            child: Text(
+                              "Completed",
+                              style: txtDis,
+                            ),
                           ),
                         ),
+                        child: TaskTile(task: tasks[index]),
                       ),
-                      child: TaskTile(task: tasks[index]),
                     ),
                   ),
                 ),
-              ),
-            );
+              );
+            }
+           else{
+             return Container();
+           }
           });
     }
   });
