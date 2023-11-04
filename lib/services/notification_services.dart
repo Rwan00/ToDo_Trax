fee26696259efb8783cb60dd862b19e08c3731ef
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2023-11-04 16:39:54 +0200

    add notification services

---
+++ lib/services/notification_services.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+
+//import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+import 'package:todo_trax/ui/views/description_view.dart';
+
+import '/models/task.dart';
+
+
+class NotifyHelper {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+  String selectedNotificationPayload = '';
+
+  final BehaviorSubject<String> selectNotificationSubject =
+  BehaviorSubject<String>();
+
+  initializeNotification() async {
+    await _configureLocalTimeZone();
+    //_configureSelectNotificationSubject();
+    // await requestIOSPermissions(flutterLocalNotificationsPlugin);
+    final DarwinInitializationSettings initializationSettingsIOS =
+    DarwinInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+      onDidReceiveLocalNotification: onDidReceiveLocalNotification,
+    );
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('icon');
+
+    final InitializationSettings initializationSettings =
+    InitializationSettings(
+      iOS: initializationSettingsIOS,
+      android: initializationSettingsAndroid,
+    );
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: onDidReceiveNotificationResponse,
+    );
+  }
+
+  void onDidReceiveNotificationResponse(
+      NotificationResponse notificationResponse) async {
+    final String? payload = notificationResponse.payload;
+    if (notificationResponse.payload != null) {
+      debugPrint('notification payload: $payload');
+    }
+    /*await Get.to(DescriptionView(
+      task: ,
+    ));*/
+  }
+
+  displayNotification({required String title, required String body}) async {
+    print('doing test');
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        'your channel id', 'your channel name',
+        channelDescription: 'your channel description',
+        importance: Importance.max,
+        priority: Priority.high);
+    var darwinPlatformChannelSpecifics = const DarwinNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: darwinPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
+  }
+
+  cancelNotification(Task task) async {
+    await flutterLocalNotificationsPlugin.cancel(task.id!);
+    print("canceled");
+  }
+  cancelAllNotification() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
+    print("All Canceled");
+  }
+
+  scheduledNotification(int hour, int minutes, Task task) async {
+    var notDetails = const NotificationDetails(
+      android: AndroidNotificationDetails(
+          'your channel id', 'your channel name',
+          channelDescription: 'your channel description'),
+    );
+
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      task.id!,
+      task.title,
+      task.description,
+      //tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
+      _nextInstanceOfTenAM(
+          hour, minutes, task.reminder!, task.repeat == 0
+          ? "Daily"
+          : task.repeat == 1
+          ? "Weekly"
+          : task.repeat == 2
+          ? "Monthly"
+          : "None", task.date!),
+      notDetails,
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+      uiLocalNotificationDateInterpretation:
+      UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.time,
+      //payload: '${task.title}|${task.note}|${task.startTime}|${task.color}',
+    );
+    return notDetails;
+  }
+
+  tz.TZDateTime _nextInstanceOfTenAM(
+      int hour, int minutes, int remind, String repeat, String date) {
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    print(now);
+
+    var formattedDate = DateFormat.yMd().parse(date);
+    final tz.TZDateTime fd = tz.TZDateTime.from(formattedDate,tz.local);
+
+    tz.TZDateTime scheduledDate =
+    tz.TZDateTime(tz.local, fd.year, fd.month, fd.day, hour, minutes);
+    print("scheduled Date: $scheduledDate");
+
+    /*scheduledDate = afterRemind(remind, scheduledDate);
+
+    if (scheduledDate.isBefore(now)) {
+      if (repeat == "Daily") {
+        scheduledDate = tz.TZDateTime(tz.local, now.year, now.month,
+            (formattedDate.day) + 1, hour, minutes);
+      }
+      if (repeat == "Weekly") {
+        scheduledDate = tz.TZDateTime(tz.local, now.year, now.month,
+            (formattedDate.day) + 7, hour, minutes);
+      }
+      if (repeat == "Monthly") {
+        scheduledDate = tz.TZDateTime(tz.local, now.year,
+            (formattedDate.month) + 1, formattedDate.day, hour, minutes);
+      }
+      scheduledDate = afterRemind(remind, scheduledDate);
+    }*/
+    print("next scheduled $scheduledDate");
+    return scheduledDate;
+  }
+
+  void requestIOSPermissions() {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+        IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+  }
+
+  Future<void> _configureLocalTimeZone() async {
+    tz.initializeTimeZones();
+    final String timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZoneName));
+  }
+
+/*   Future selectNotification(String? payload) async {
+    if (payload != null) {
+      //selectedNotificationPayload = "The best";
+      selectNotificationSubject.add(payload);
+      print('notification payload: $payload');
+    } else {
+      print("Notification Done");
+    }
+    Get.to(() => SecondScreen(selectedNotificationPayload));
+  } */
+
+//Older IOS
+  Future onDidReceiveLocalNotification(
+      int id, String? title, String? body, String? payload) async {
+    // display a dialog with the notification details, tap ok to go to another page
+    /* showDialog(
+      context: context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: const Text('Title'),
+        content: const Text('Body'),
+        actions: [
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            child: const Text('Ok'),
+            onPressed: () async {
+              Navigator.of(context, rootNavigator: true).pop();
+              await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Container(color: Colors.white),
+                ),
+              );
+            },
+          )
+        ],
+      ),
+    );
+ */
+    Get.dialog(Text(body!));
+  }
+
+  /*void _configureSelectNotificationSubject() {
+    selectNotificationSubject.stream.listen() async {
+      debugPrint('My payload is $payload');
+      await Get.to(() => const DescriptionView(
+        task: null,
+      ));
+    });
+  }*/
+
+  tz.TZDateTime afterRemind(int remind, tz.TZDateTime scheduledDate) {
+    if (remind == 5) {
+      scheduledDate = scheduledDate.subtract(const Duration(minutes: 5));
+    }
+    if (remind == 10) {
+      scheduledDate = scheduledDate.subtract(const Duration(minutes: 10));
+    }
+    if (remind == 15) {
+      scheduledDate = scheduledDate.subtract(const Duration(minutes: 15));
+    }
+    if (remind == 20) {
+      scheduledDate = scheduledDate.subtract(const Duration(minutes: 20));
+    }
+    return scheduledDate;
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_local_notifications
+import flutter_native_timezone
 import modal_progress_hud_nsn
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
   ModalProgressHudNsnPlugin.register(with: registry.registrar(forPlugin: "ModalProgressHudNsnPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
