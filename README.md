7067c808b74c2d9800ab732f15a4c4f394536ae5
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-02-21 20:25:15 +0200

    update readme file

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ This project is a starting point for a Flutter application.
 
 A few resources to get you started if this is your first Flutter project:
 
+- [ToDo Trax "Simple todo app"](assets/images/todo-trax.mp4)
+
+
+
 - [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
 - [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
--- lib/simple_bloc_observer.dart
+++ lib/simple_bloc_observer.dart
@@ -1,35 +1,44 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc/bloc.dart';
 
-class SimpleBlocObserver implements BlocObserver{
+class SimpleBlocObserver extends BlocObserver {
+  const SimpleBlocObserver();
+
   @override
-  void onChange(BlocBase bloc, Change change) {
-    debugPrint("changes = $change");
+  void onCreate(BlocBase<dynamic> bloc) {
+    super.onCreate(bloc);
+    print('onCreate -- bloc: ${bloc.runtimeType}');
   }
 
   @override
-  void onClose(BlocBase bloc) {
-    debugPrint("close = $bloc");
+  void onEvent(Bloc<dynamic, dynamic> bloc, Object? event) {
+    super.onEvent(bloc, event);
+    print('onEvent -- bloc: ${bloc.runtimeType}, event: $event');
   }
 
   @override
-  void onCreate(BlocBase bloc) {
-    debugPrint("create = $bloc");
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    print('onChange -- bloc: ${bloc.runtimeType}, change: $change');
   }
 
   @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    // TODO: implement onError
+  void onTransition(
+      Bloc<dynamic, dynamic> bloc,
+      Transition<dynamic, dynamic> transition,
+      ) {
+    super.onTransition(bloc, transition);
+    print('onTransition -- bloc: ${bloc.runtimeType}, transition: $transition');
   }
 
   @override
-  void onEvent(Bloc bloc, Object? event) {
-    // TODO: implement onEvent
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    print('onError -- bloc: ${bloc.runtimeType}, error: $error');
+    super.onError(bloc, error, stackTrace);
   }
 
   @override
-  void onTransition(Bloc bloc, Transition transition) {
-    // TODO: implement onTransition
+  void onClose(BlocBase<dynamic> bloc) {
+    super.onClose(bloc);
+    print('onClose -- bloc: ${bloc.runtimeType}');
   }
-
 }
